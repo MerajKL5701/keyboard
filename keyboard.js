a7db822d88a9b0902e6bc80146b799cf722be110
MerajKL5701 <khanmeraj5701@gmail.com>
2023-06-26 13:08:51 +0530

    "hopefully the final change"

--- keyboard.js
+++ keyboard.js
@@ -8,6 +8,7 @@ const mm = document.getElementsByClassName("mm")[0]
 const WPM = document.getElementsByClassName("WPM")[0]
 const volumeRange = document.getElementsByClassName("volumeRange")[0]
 let     startSupport = 1;
+let errorsupport = 1;
 let minutes = 0;
 let WPMS = []
 let charIndex = 0;
@@ -19,13 +20,13 @@ let Right = 0;
 const audioElement = document.getElementsByClassName("audio")[0];
 let Left = 0;
 let RandText = [
-  "y","0", "kitagawa","Tiger","naruto","luffy","erenyeager", "yep"
+  "kitagawa","Tiger","naruto","luffy","erenyeager", "yep","aqua","ruby","zoro","sasuke","saitama","marinKitagawa",
+  "itachi","madara","shinobu","ai","light","garou","akane","Mikasa","zenitsu"
 ];
-
 
 let ms = 0;
 // text.style.opacity = "0";
-
+volumeRange.value = 100
 const Button = document.getElementsByClassName("key");
 for (let i = 0; i < Button.length; i++) {
   Button[i].classList.add(Button[i].innerHTML.trim());
@@ -40,7 +41,7 @@ for (let i = 0; i < Button.length; i++) {
 // text.focus();
 window.addEventListener("keydown", ((e) => {
     // Accurate.style.opacity = "0"
-    if (e.keyCode <= 90 && e.keyCode >= 60)
+    // if (e.keyCode <= 90 && e.keyCode >= 60)
     text.focus();
 
    if (startSupport === 1) {  
@@ -58,7 +59,10 @@ text.addEventListener("input", function () {
   KeyPress();
 
   setTimeout(() => {
-    checkInput();
+    if (errorsupport < 2){
+
+      checkInput();
+    }
     scorer()
   }, 1);
   
@@ -73,10 +77,21 @@ function checkInput() {
   const checker = document.getElementById("checker");
   if (Textvalue !== checker.innerHTML.charAt(0))
   {
-    checker.classList.add("error")
+    document.documentElement.style.setProperty("--sliver","0, 100%, 50%")
+    checker.classList.add("error");
+    errorsupport = 2
+    
+    text.value = ''
     setTimeout(() => {
+      console.log("textbox value" + checker.innerHTML.charAt(0) + text.value.toUpperCase()  )
+      errorsupport = 1
+    }, 1000);
+    setTimeout(() => {
+     
+      console.log("remove console")
+      document.documentElement.style.setProperty("--sliver","0, 0%, 75%")
       checker.classList.remove("error")
-    },100)
+    },1000)
     beep.volume = volumeRange.value / 100
     beep.play()
     Left++;
@@ -106,7 +121,7 @@ function checkInput() {
   let AnimateSupport = 0;
   function animator(){
     let sakura = []
-    for (i = 40; i<60;i++){
+    for (i = 40; i<80;i++){
       sakura.push(i)
     }
     let kakashi = []
@@ -168,11 +183,7 @@ function checkInput() {
       checker.innerHTML = RandText[RandTextIndex].toUpperCase();
       
     } 
-    function enter(E) {
-      text.value = E;
-      KeyPress();
-      checkInput();
-    }
+  
     function KeyPress(){
       for (let i = 0; i < Button.length; i++) {
         const Textvalue = text.value.toUpperCase();
@@ -235,4 +246,8 @@ function checkInput() {
         Accurate.innerHTML = idiot.toFixed(2) + "%"
         
       } 
-    
+      function enter(E) {
+        text.value = E;
+        KeyPress();
+        checkInput();
+      }
